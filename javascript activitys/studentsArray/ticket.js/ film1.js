ef2c687405cc4e3292c8305e4b16147f40c649d4
MiDhunmadhu1111 <midhunm.ceecs24@duk.ac.in>
2024-06-07 16:19:27 +0530

    first commit

---
+++ javascript activitys/studentsArray/ticket.js/ film1.js
@@ -0,0 +1,7 @@
+function sub({
+    a=document.getElementById("name").value;
+    b=document.getElementById("film").value;
+    c=document.getElementById("nos").value;
+    c=Number.parseInt(c);
+    s=a+"booked""
+})
